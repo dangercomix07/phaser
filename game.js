87dc34312a5977c200954ed2d0b2dd811855afeb
Ameya <ameya.marakarkandy@gmail.com>
2022-01-31 20:12:38 +0530

    trial commit

---
+++ game.js
@@ -0,0 +1,8 @@
+const game = new Phaser.Game(800,600,Phaser.AUTO,'',{
+    preload: preload,
+    create: create,
+    update: update,
+})
+function preload (){}
+function create (){}
+function update (){}
